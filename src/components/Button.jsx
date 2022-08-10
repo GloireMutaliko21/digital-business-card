5669adeb452a2408fd76ddff05581136ddf2a0a1
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-08-10 18:46:26 +0200

    adding button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Button = ({ icon, text, color, bgColor }) => {
+    return (
+        <button type='button' style={{
+            width: "100%",
+            marginInline: 10,
+            marginTop: 6,
+            width: 115,
+            height: 34,
+            backgroundColor: bgColor,
+            color, display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            borderRadius: 6,
+            fontSize: 14,
+            border: "0px"
+        }}>
+            {icon}
+            {text}
+        </button>
+    )
+}
+
+export default Button
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import image from "../assets/image.png";
 import '../styles/main.css'
+import Button from './Button';
 import Description from './Description';
+import { HiMail } from "react-icons/hi";
+import { AiFillLinkedin } from "react-icons/ai";
 
 const MainContent = () => {
     return (
@@ -9,6 +12,21 @@ const MainContent = () => {
             <img src={image} alt="" />
             <div className='body'>
                 <Description />
+                <div style={{
+                    display: "flex",
+                    // justifyContent: "space-around"
+                }}>
+                    <Button
+                        icon={<HiMail style={{ fontSize: 16 }} />}
+                        text="Email"
+                    />
+                    <Button
+                        icon={<AiFillLinkedin style={{ fontSize: 16 }} />}
+                        text="Linkedin"
+                        bgColor="#5093E2"
+                        color="white"
+                    />
+                </div>
             </div>
         </div>
     )
